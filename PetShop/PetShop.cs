b0304441389e777451e0cfccd0d774775f2f17f4
Jqb1437 <jgwzd2684@protonmail.com>
2022-11-16 19:17:00 +0100

    zad2

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -28,24 +28,52 @@ namespace Training.DomainClasses
             _petsInTheStore.Add(newPet);
         }
 
-        public IEnumerable<Pet> AllCats()
-        {
-
-            foreach (var pet in _petsInTheStore)
-            {
-                if (pet.species == Species.Cat)
-                {
-                    yield return pet;
-                }
-            }
-
-        }
+        public IEnumerable<Pet> AllCats() => SelectAllThatSatisfy(pet => pet.species == Species.Cat);
 
         public IEnumerable<Pet> AllPetsSortedByName()
         {
             var ret = new List<Pet>(_petsInTheStore);
             ret.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return ret;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits() =>
+            SelectAllThatSatisfy(pet => pet.species == Species.Rabbit || pet.yearOfBirth > 2011);
+
+
+        public IEnumerable<Pet> AllMice() => SelectAllThatSatisfy(pet => pet.species == Species.Mouse);
+
+
+        public IEnumerable<Pet> AllFemalePets() => SelectAllThatSatisfy(pet => pet.sex == Sex.Female);
+
+
+        public IEnumerable<Pet> AllCatsOrDogs() =>
+            SelectAllThatSatisfy(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+
+
+        public IEnumerable<Pet> AllPetsButNotMice() => SelectAllThatSatisfy(x => x.species != Species.Mouse);
+
+
+        public IEnumerable<Pet> AllPetsBornAfter2010() => SelectAllThatSatisfy(pet => pet.yearOfBirth > 2010);
+
+
+        public IEnumerable<Pet> AllDogsBornAfter2010() =>
+            SelectAllThatSatisfy(pet => pet.species == Species.Dog && pet.yearOfBirth > 2010);
+
+
+        public IEnumerable<Pet> AllMaleDogs() =>
+            SelectAllThatSatisfy(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
+
+
+        private IEnumerable<Pet> SelectAllThatSatisfy(Func<Pet, bool> condition)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (condition(pet))
+                {
+                    yield return pet;
+                }
+            }
         }
     }
 }
