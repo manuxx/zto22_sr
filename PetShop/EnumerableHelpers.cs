de298ac1dca5924824cf1a5e796437967832876c
Jqb1437 <jgwzd2684@protonmail.com>
2022-11-16 19:59:21 +0100

    zad 3 i 4

---
+++ PetShop/EnumerableHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    static internal class EnumerableHelpers
+    {
+        public static IEnumerable<TItem> GetEnumerable<TItem>(this IEnumerable<TItem> enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                yield return item;
+            }
+        }
+    }
+}
--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Training.DomainClasses
 {
@@ -14,10 +15,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPets()
         {
-            foreach (var pet in _petsInTheStore)
-            {
-                yield return pet;
-            }
+            return new ReadOnlySet<Pet>(_petsInTheStore.GetEnumerable());
         }
 
         public void Add(Pet newPet)
