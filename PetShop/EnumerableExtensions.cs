dde6bf1a79226e4e90e8f97ea14cacb6ac3f1e9a
manuxx <manus@pg.gda.pl>
2022-11-23 18:04:15 +0100

    poczatek nowej skladni Criteria

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -22,4 +22,14 @@ static internal class EnumerableExtensions
             }
         }
     }
+    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
+    {
+        foreach (var item in items)
+        {
+            if (criteria.IsSatisfiedBy(item))
+            {
+                yield return item;
+            }
+        }
+    }
 }
