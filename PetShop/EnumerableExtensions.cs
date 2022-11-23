4670b05d4b12bed08d1ca1f855a12e29835ad0ad
311Volt <113amper@gmail.com>
2022-11-23 17:55:25 +0100

    generic filter extension

---
+++ PetShop/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    internal static class EnumerableUtils
+    {
+        public static IEnumerable<T> Launder<T>(this IEnumerable<T> enumerable)
+        {
+            foreach (var item in enumerable)
+            {
+                yield return item;
+            }
+        }
+        public static IEnumerable<T> SelectAllThatSatisfy<T>(this IEnumerable<T> items, Predicate<T> pred)
+        {
+            foreach (var item in items)
+            {
+                if (pred(item))
+                    yield return item;
+            }
+        }
+    }
+}
