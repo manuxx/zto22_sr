a9b970ddc3ce7a76d506b783d59d472248dacf82
manuxx <manus@pg.gda.pl>
2022-11-30 17:29:01 +0100

    usuwamy zbedny przestarzaly kod

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -12,11 +12,6 @@ static internal class EnumerableExtensions
         }
     }
 
-    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
-    {
-        return items.ThatSatisfy(new AnonymousCriteria<TItem>(condition));
-    }
-
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, ICriteria<TItem> criteria)
     {
         foreach (var item in items)
