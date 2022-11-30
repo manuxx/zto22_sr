89972e86d2ad179cb4e58d44955d77f927ef5213
aniabuto <aniabuto@wp.pl>
2022-11-30 17:59:44 +0100

    creating criteria builder

--- PetShop/BinaryCriteria.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Training.DomainClasses
-{
-    public abstract class BinaryCriteria<T> : ICriteria<T>
-    {
-        protected ICriteria<T> _criteria1;
-        protected ICriteria<T> _criteria2;
-        public abstract bool IsSatisfiedBy(T item);
-    }
-}
